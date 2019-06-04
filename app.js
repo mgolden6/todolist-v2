6bf80aab2a031e87a049532561cf38959407d5a6
Golden <goldenm4@BKSQ-Q2E7-MAC.hsd1.pa.comcast.net>
2019-06-04 16:43:13 -0400

    add ejs

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.set("view engine", "ejs");
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        month: "long",
+        day: "numeric"
+    };
+
+    let weekday = today.toLocaleDateString("en-US", options);
+    res.render("list", {
+        kindOfDay: weekday
+    });
+});
 
 app.listen(port, function () {
     console.log("app running on port " + port);
